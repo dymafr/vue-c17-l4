98826da4a90817b8b7cb1235c76a9900cb40ee9b
dymafr <erwan1309@hotmail.com>
2022-03-30 19:24:05 +0200

    1 file modified

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,39 +8,64 @@ const emit = defineEmits<{
 }>();
 </script>
 
+
 <template>
-  <header class="px-20 d-flex flex-row align-items-center">
-    <a href="#" class="d-flex flex-row align-items-center mr-20">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
-      />
-      <span class="logo">Dyma</span>
-    </a>
-    <ul class="d-flex flex-row flex-fill">
-      <li class="mr-10">
-        <a
-          :class="{ active: page === 'Boutique' }"
-          @click="emit('navigate', 'Boutique')"
-          >Boutique</a
-        >
-      </li>
-      <li>
-        <a
-          :class="{ active: page === 'Admin' }"
-          @click="emit('navigate', 'Admin')"
-          >Admin</a
-        >
-      </li>
-    </ul>
-    <ul class="d-flex flex-row">
-      <li class="mr-10">
-        <a href="#">Inscription</a>
-      </li>
-      <li>
-        <a href="#">Connexion</a>
-      </li>
-    </ul>
-  </header>
+    <header class="px-20 d-flex flex-row align-items-center">
+        <a href="#" class="d-flex flex-row align-items-center mr-20">
+            <img src="../assets/logo.svg" />
+            <span class="logo">Dyma</span>
+        </a>
+        <div class="d-flex flex-row align-items-center flex-fill actions-container">
+            <ul class="d-flex flex-row flex-fill hide-xs flex-fill">
+                <li class="mr-10">
+                    <a
+                        :class="{ active: page === 'Boutique' }"
+                        @click="emit('navigate', 'Boutique')"
+                    >Boutique</a>
+                </li>
+                <li>
+                    <a
+                        :class="{ active: page === 'Admin' }"
+                        @click="emit('navigate', 'Admin')"
+                    >Admin</a>
+                </li>
+            </ul>
+            <ul class="d-flex flex-row hide-xs">
+                <li class="mr-10">
+                    <a href="#">Inscription</a>
+                </li>
+                <li>
+                    <a href="#">Connexion</a>
+                </li>
+            </ul>
+            <div class="menu-xs-container">
+                <Calc :open="state.open" @close="state.open = false" :transparent="true" />
+                <i @click="state.open = !state.open" class="fa-solid fa-bars show-xs"></i>
+                <Transition>
+                    <ul @click="state.open = false" v-if="state.open" class="menu card">
+                        <li>
+                            <a
+                                :class="{ active: page === 'Boutique' }"
+                                @click="emit('navigate', 'Boutique')"
+                            >Boutique</a>
+                        </li>
+                        <li>
+                            <a
+                                :class="{ active: page === 'Admin' }"
+                                @click="emit('navigate', 'Admin')"
+                            >Admin</a>
+                        </li>
+                        <li>
+                            <a href="#">Inscription</a>
+                        </li>
+                        <li>
+                            <a href="#">Connexion</a>
+                        </li>
+                    </ul>
+                </Transition>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
